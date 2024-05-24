7943e6577be10fe55da56580e15320894e2016c5
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-24 23:12:35 +0530

    Login jsx file updated.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,6 @@
 import React from 'react'
 
 const Login = () => {
-    const client_id = "020989549ba041579c0eb90c5014562e";
-    const redirect_uri = "http://localhost:5173/";
-    const api_uri = "https://accounts.spotify.com/authorize";
-    const scope = [
-      'user-read-email',
-      'user-read-private',
-      'user-modify-playback-state',
-      'user-read-playback-state',
-      'user-read-currently-playing',
-      'user-read-recently-played',
-      'user-read-playback-position',
-      'user-top-read'
-    ];
-
-
   return (
     <section className='bg-[black] h-full text-white'>
 
